14a15c3f16b7d7d63532073800fc568bc654b2cc
liu <50645625+flowyone@users.noreply.github.com>
2019-05-15 10:18:00 +0800

    Update testApi.py

--- testApi.py
+++ testApi.py
@@ -17,7 +17,7 @@ class Run(Logout):
  # 科室判断节口
     def test_keshi_api(self):
         param = {"s": "白癜风怎么治疗", "a": 2}
-        url = 'http://dept.120.net/api/auto'
+        url = 'http://domain.com/api/auto'
         r = requests.get(url, params=param)
         r = r.json()
         self.assertEqual(r["status"], 200)
@@ -25,19 +25,19 @@ class Run(Logout):
  # 关键词相关接口
     def test_relate(self):
         param = {"word": "白癜风"}
-        url = 'https://news.120ask.com/api/GetListByWord'
+        url = 'https://domain.com/api/GetListByWord'
         r = requests.get(url, params=param)
         r = r.json()
         self.assertIsNotNone(r)
  # 最新新闻接口
     def test_news(self):
-        url = 'https://news.120ask.com/api/news'
+        url = 'https://domain.com/api/news'
         r = requests.get(url)
         r = r.json()
         self.assertIsNotNone(r)
  # 最新新闻接口
     def test_yao_news(self):
-        url = 'https://news.120ask.com/api/YaoNews'
+        url = 'https://domain.com/api/YaoNews'
         r = requests.get(url)
         r = r.json()
         self.assertIsNotNone(r)
